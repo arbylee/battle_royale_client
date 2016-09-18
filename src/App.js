6322ba4bfc32725b7b4eae95e81cde8b3f2d1b5d
Richard Lee <rblee88@gmail.com>
2016-09-19 09:24:04 +1000

    Pull out user actions window into a component

--- src/App.js
+++ src/App.js
@@ -1,65 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import ServerLog from './ServerLog.js';
+import UserActions from './UserActions.js';
 import { connect as io } from 'socket.io-client';
 
 var socket = io('http://localhost:3001');
 socket.connect('http://localhost/foobar', { autoConnect: true});
 
 class App extends Component {
-  componentDidMount() {
-    const that = this;
-
-    socket.on('user state', function(msg){
-      that.setState({gold: msg});
-    });
-  }
-
-  constructor(props) {
-    super(props);
-    this.handleActionSubmit = this.handleActionSubmit.bind(this);
-    this.handleBetChange = this.handleBetChange.bind(this);
-
-    this.state = {
-      gold: 0,
-      selectedTarget: "1"
-    };
-  }
-
-  handleActionSubmit(event) {
-    event.preventDefault();
-    socket.emit('user action', this.state.selectedTarget);
-    this.setState({chatInputValue: ''});
-  }
-
-  handleBetChange(event){
-    this.setState({selectedTarget: event.target.value})
-  }
-
   render() {
     return (
       <div className="container">
         <ServerLog socket={socket} />
-        <div className="user-actions">
-          <div className="gold">Gold: {this.state.gold}</div>
-          <form className="action-form" action="" onSubmit={this.handleActionSubmit}>
-            <select value={this.state.selectedTarget} onChange={this.handleBetChange}>
-              <option value="1">Jam</option>
-              <option value="2">Morning Fresh</option>
-              <option value="3">Lune</option>
-              <option value="4">Pixie</option>
-              <option value="5">Madden</option>
-              <option value="6">Oscar</option>
-              <option value="7">Vitesse</option>
-              <option value="8">Pharoah</option>
-              <option value="9">Elsa</option>
-            </select>
-            <button>Send</button>
-          </form>
-        </div>
+        <UserActions socket={socket} />
       </div>
     );
   }
 }
 
 export default App;
+
---
+++ src/UserActions.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class UserActions extends Component {
+  constructor(props) {
+    super(props);
+    this.handleActionSubmit = this.handleActionSubmit.bind(this);
+    this.handleBetChange = this.handleBetChange.bind(this);
+
+    this.state = {
+      gold: 0,
+      selectedTarget: "1"
+    };
+  }
+
+  componentDidMount() {
+    const that = this;
+
+    that.props.socket.on('user state', function(msg){
+      that.setState({gold: msg});
+    });
+  }
+
+  handleActionSubmit(event) {
+    event.preventDefault();
+    this.props.socket.emit('user action', this.state.selectedTarget);
+    this.setState({chatInputValue: ''});
+  }
+
+  handleBetChange(event){
+    this.setState({selectedTarget: event.target.value})
+  }
+
+  render() {
+    return (
+      <div className="user-actions">
+        <div className="gold">Gold: {this.state.gold}</div>
+        <form className="action-form" action="" onSubmit={this.handleActionSubmit}>
+          <select value={this.state.selectedTarget} onChange={this.handleBetChange}>
+            <option value="1">Jam</option>
+            <option value="2">Morning Fresh</option>
+            <option value="3">Lune</option>
+            <option value="4">Pixie</option>
+            <option value="5">Madden</option>
+            <option value="6">Oscar</option>
+            <option value="7">Vitesse</option>
+            <option value="8">Pharoah</option>
+            <option value="9">Elsa</option>
+          </select>
+          <button>Send</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default UserActions;
+
